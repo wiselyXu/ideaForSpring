859c2d4ba8f3c5d0b25391e338fb8d6e3c0fa0be
wiselyXu <twtqmq1@163.com>
2018-01-14 22:20:51 +0800

    first commit

---
+++ src/com/lanhai/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.lanhai.hello;
+
+public class HelloWorld {
+    private String name;
+
+    public void setName(String aa){
+        this.name=aa;
+    }
+
+    public void sayHi(){
+        System.out.print("hello "+ name);
+    }
+}
---
+++ src/com/lanhai/hello/Usage.java
@@ -0,0 +1,11 @@
+package com.lanhai.hello;
+
+public class Usage {
+    public static void main(){
+        HelloWorld helloWorld= new HelloWorld();
+
+        helloWorld.setName("tomas");
+        helloWorld.sayHi();
+
+    }
+}
